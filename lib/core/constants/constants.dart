f253bb4bed814c691f6c23086b6acd47474af643
amirhossein jalali <rimaj@amirhosseins-MacBook-Air.local>
2023-06-17 12:10:51 +0330

    config firebase for android and ios

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -1,5 +1,5 @@
 class IMAGES {
   static const backgroundWallpaper1 = "assets/images/background_wallpaper.png";
-  static const backgroundWallpaper2 = "assets/images/background.png";
+  static const backgroundWallpaper2 = "assets/images/background.jpg";
   static const profileDefault = "assets/images/profile_default.png";
 }
